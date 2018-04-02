53b5329d6802875005fd0fc3c87c9e8949bafb4b
prshntattri1 <prshntattri1@gmail.com>
2018-04-03 00:20:56 +0530

    user.dir Test for Anmol

---
+++ Anmol/src/TestUserDirProperty/UserDirPropertyTest.java
@@ -0,0 +1,10 @@
+package TestUserDirProperty;
+
+public class UserDirPropertyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(System.getProperty("user.dir"));
+	}
+
+}
